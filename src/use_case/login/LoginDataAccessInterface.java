098d82c5e4788d29b8caa59969f02d8ad05ee645
FionaYYX <fionayiyang.xu@mail.utoronto.ca>
2023-10-23 18:50:39 -0400

    changes to data access

--- src/use_case/login/LoginDataAccessInterface.java
+++ src/use_case/login/LoginDataAccessInterface.java
@@ -1,5 +1,12 @@
 package use_case.login;
 
+import entity.User;
+
 public interface LoginDataAccessInterface {
 
+    boolean existsByUsername(String username);
+
+    boolean matchesByPassword(String username, String password);
+
+    User getUser(String username);
 }

20d37ba7357a35dca275a9d58a6d4cce8b2616f5
Gabe Payumo <gabe.payumo@mail.utoronto.ca>
2023-10-26 13:14:09 -0400

    implementation of LoginController

---
+++ src/interface_adapter/login/LoginController.java
@@ -0,0 +1,17 @@
+package interface_adapter.login;
+
+import use_case.login.LoginInputData;
+
+public class LoginController {
+
+    final LoginInputBoundary loginInteractor;
+
+    public LoginController(LoginInputBoundary loginInteractor) {
+        this.loginInteractor = loginInteractor;
+    }
+
+    public void execute(String username, String password) {
+        LoginInputData loginInputData = new LogiInputData(username, password);
+        loginInteractor.execute(loginInputData);
+    }
+}

b2582dd1646ba7fa844128a851ff9645da8641d1
emmLiao <shizhuo.liao@mail.utoronto.ca>
2023-10-26 13:10:36 -0400

    update on login and logged

--- src/interface_adapter/logged_in/LoggedInViewModel.java
+++ src/interface_adapter/logged_in/LoggedInViewModel.java
@@ -1,2 +1,35 @@
-package interface_adapter.logged_in;public class LoggedInViewModel {
+package interface_adapter.logged_in;
+
+import interface_adapter.ViewModel;
+import interface_adapter.login.LoginState;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class LoggedInViewModel extends ViewModel{
+
+    private  LoggedInState loggedInState = new LoggedInState();
+
+    public LoggedInViewModel() {
+        super(("Select your note"));
+    }
+
+    public void setState(LoggedInState loggedInState){
+        this.loggedInState = loggedInState;
+    }
+
+    public LoggedInState getState(){
+        return loggedInState;
+    }
+
+    private  final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void firePropertyChange() {
+        support.firePropertyChange("state",null, this.loggedInState);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 }

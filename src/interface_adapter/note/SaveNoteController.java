8e79235b0c9b37f07165a38bb1210639c2110a7e
Gabe Payumo <gabe.payumo@mail.utoronto.ca>
2023-11-03 19:50:34 -0400

    Implemented the SaveNoteController.java file

--- src/interface_adapter/note/SaveNoteController.java
+++ src/interface_adapter/note/SaveNoteController.java
@@ -5,6 +5,11 @@ import use_case.save_note.SaveNoteBoundary;
 public class SaveNoteController {
     final SaveNoteInputBoundary saveNoteInteractor;
     public SaveNoteController(SaveNoteInputBoundary saveNoteInteractor) {
-        this.SaveNoteInteractor =
+        this.saveNoteInteractor =saveNoteInteractor;
+    }
+
+    public void execute(String username, String noteData, String fileName, LocalDateTime createdTime) {
+        SaveNoteInputData saveNoteInputData = new SaveNoteInputeData(username, noteData, fileName, createdTime);
+        saveNoteInteractor.execute(saveNoteInputData);
     }
 }

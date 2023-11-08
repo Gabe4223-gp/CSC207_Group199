8726d851a98aaf67a3ae0ac1a7de527b709e7a40
reesh <reeshav.kumar@mail.utoronto.ca>
2023-11-08 01:39:31 -0500

    Working Connector Class for the database

--- src/data_access/DBConnector.java
+++ src/data_access/DBConnector.java
@@ -1,8 +1,14 @@
 package data_access;
+import com.mysql.cj.xdevapi.JsonArray;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.sql.*;
+import java.time.*;
 
 public class DBConnector {
-    Connection connection;
+    private Connection connection;
+    private JSONArray allUsers = new JSONArray();
     public DBConnector(){
         String url = "jdbc:mysql://sql5.freemysqlhosting.net:3306/sql5659838";
         String username = "sql5659838";
@@ -10,17 +16,73 @@ public class DBConnector {
         try {
             connection = DriverManager.getConnection(url, username, password);
             Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery("SELECT * FROM Users");
-            while (result.next()){
-                System.out.println(result.getString(1) + " " + result.getString(2));
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Users");
+
+            /*
+            * Convert the JDBC resultset to a JSONArray to be able to access data easily
+            *
+            * */
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+            while (resultSet.next()){
+                int numColumns = resultSetMetaData.getColumnCount();
+                JSONObject obj = new JSONObject();
+                for (int i = 1; i <= numColumns ; i++) {
+                    String colName = resultSetMetaData.getColumnName(i);
+                    obj.put(colName, resultSet.getObject(colName));
+                }
+                allUsers.put(obj);
             }
+
         }catch (Exception e){
             System.out.println(e);
         }
     }
+    public JSONObject getUserObj(String username){
+        for(Object obj: this.allUsers){
+            JSONObject thisObj = (JSONObject)obj;
+            String thisUsername = (String) thisObj.get("Username");
+            if (thisUsername.equals(username)){
+                return thisObj;
+            }
+        }
+        return null;
+    }
 
-    public static void main(String[] args) {
+    public boolean checkLoginCredentials(String username, String password){
+        JSONObject userObj = getUserObj(username);
+        if(userObj == null){
+            return false;
+        }else {
+            if (userObj.get("Password").equals(password)){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
 
-        DBConnector db = new DBConnector();
+    public boolean createUser(String username, String password){
+        JSONObject userObj = getUserObj(username);
+        if(userObj != null){
+            return false;
+        }
+        try{
+            Statement insertStatement = this.connection.createStatement();
+            String sqlQuery = "INSERT INTO `Users`(`Username`, `Password`, `Created_On`) VALUES ('"+
+                    username + "','" +
+                    password + "', " +
+                    "CURRENT_TIMESTAMP())";
+            insertStatement.execute(sqlQuery);
+            return true;
+        }catch (Exception e){
+            System.out.println(e);
+            return false;
+        }
+
+    }
+
+    public void DBClose() throws SQLException {
+        this.connection.close();
     }
 }

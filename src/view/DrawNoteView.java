3a9271a323922966f51bd054f77349c59a70aadd
Gabe Payumo <gabe.payumo@mail.utoronto.ca>
2023-11-17 17:12:32 -0500

    Created a public but empty class of DrawNoteView to get started with the DrawNote useCase.

--- src/view/DrawNoteView.java
+++ src/view/DrawNoteView.java
@@ -1,4 +1,17 @@
 package view;
 
-public class DrawNoteView {
+import interface_adapter.draw_note.DrawNoteViewModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
+
+public class DrawNoteView extends JPanel implements ActionListener, PropertyChangeListener {
+
+    public final String viewTitle = "drawings";
+    private final DrawNoteViewModel drawNoteViewModel;
+
+
+
 }

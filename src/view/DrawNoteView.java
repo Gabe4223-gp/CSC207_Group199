eae81133a407a40478ed802f859d1bf07df0c253
Gabe Payumo <gabe.payumo@mail.utoronto.ca>
2023-11-17 17:13:53 -0500

    Updated commit on DrawNoteView.java, currently has the public classes and methods but not yet implemented. To be continued.

--- src/view/DrawNoteView.java
+++ src/view/DrawNoteView.java
@@ -3,8 +3,9 @@ package view;
 import interface_adapter.draw_note.DrawNoteViewModel;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 public class DrawNoteView extends JPanel implements ActionListener, PropertyChangeListener {
@@ -13,5 +14,17 @@ public class DrawNoteView extends JPanel implements ActionListener, PropertyChan
     private final DrawNoteViewModel drawNoteViewModel;
 
 
+    public DrawNoteView(DrawNoteViewModel drawNoteViewModel) {
+        this.drawNoteViewModel = drawNoteViewModel;
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
